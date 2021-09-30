d899525b2929b238fcffe5d6778cac2939e80ce5
Simon Frankau <sgf@arbitrary.name>
2021-09-30 22:21:44 +0100

    First pass at using deconvolution, not fully hooked in yet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{AppSettings, ArgEnum, Clap};
+use rustfft::{FftPlanner, FftDirection, num_complex::Complex64};
 use image::{GrayImage, Pixel};
 use nalgebra::base::{DMatrix, DVector};
 use std::path::Path;
@@ -439,9 +440,82 @@ fn reconstruct_convolution(scan: &TomoScan, width: usize, height: usize) -> Tomo
     TomoImage { width, height, data: recon_as_u8 }
 }
 
+// We're not going to do any fancy real-valued optimisations for the
+// FFT, we simply convert to and from complex numbers.
+fn to_complex(v: &[f64]) -> Vec<Complex64> {
+    v.iter().map(|x| Complex64::new(*x, 0.0)).collect()
+}
+
+// Doing a clever real-to-complex conversion for the FFTs would allow
+// us to define away the error case of the complex component being
+// non-trivial, but we're keeping things simple.
+fn from_complex(v: &[Complex64]) -> Vec<f64> {
+    const EPSILON: f64 = 1e-7;
+    assert!(v.iter().map(|z| z.im.abs()).max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap() < EPSILON);
+    v.iter().map(|x| x.re).collect()
+}
+
+fn transpose<T: Copy>(width: usize, height: usize, data: &[T]) -> Vec<T> {
+    assert_eq!(width * height, data.len());
+
+    let mut res = Vec::with_capacity(width * height);
+    // Bounds in reverse to what you might expect, since we're transposing.
+    for y in 0..width {
+        for x in 0..height {
+            res.push(data[x * width + y]);
+        }
+    }
+    res
+}
+
+// Perform a 2D FFT
+fn fourier_transform(width: usize, height: usize, data: &[Complex64], dir: FftDirection) -> Vec<Complex64> {
+    assert_eq!(width * height, data.len());
+
+    let zero = Complex64::new(0.0, 0.0);
+
+    let mut matrix = data.iter().copied().collect::<Vec<_>>();
+
+    // FFT pass in width direction
+    {
+        let mut planner = FftPlanner::new();
+        let fft = planner.plan_fft(width, dir);
+        let mut scratch = vec![zero; width];
+        fft.process_with_scratch(&mut matrix, &mut scratch);
+    }
+
+    matrix = transpose(width, height, &matrix);
+
+    // FFT pass in height direction
+    {
+        let mut planner = FftPlanner::new();
+        let fft = planner.plan_fft(height, dir);
+        let mut scratch = vec![zero; height];
+        fft.process_with_scratch(&mut matrix, &mut scratch);
+    }
+
+    transpose(height, width, &matrix)
+}
+
+// The way convolution works means the deconvolved image is shifted by
+// an amount equivalent to the coordinates of the centre of the filter.
+// "shift" can undo this.
+
+fn shift<T: Copy>(width: usize, height: usize, data: &[T], x_shift: usize, y_shift: usize) -> Vec<T> {
+    let mut res = Vec::with_capacity(width * height);
+    for y in 0..height {
+        for x in 0..width {
+            let src_y = (y + y_shift) % height;
+            let src_x = (x + x_shift) % width;
+            res.push(data[src_y * width + src_x]);
+        }
+    }
+    res
+}
+
 // TODO: Implement some tests
 // * Implement convolution via FFT, check it works the same.
-// * Generate the deconvolution filter, test end-to-end.
+// * Improve accuracy of deconvolution.
 
 ////////////////////////////////////////////////////////////////////////
 // Main entry point
@@ -1115,5 +1189,133 @@ mod tests {
         // around 1% seems good enough to me! I think the algorithms
         // do pretty much match.
         assert!(error < 0.016);
+    }
+
+    #[test]
+    fn test_transpose() {
+        let width = 5;
+        let height = 7;
+        let v = (0..(height * width)).collect::<Vec<_>>();
+
+        let vt = transpose(width, height, &v);
+
+        for y in 0..height {
+            for x in 0..width {
+                assert_eq!(v[y * width + x], vt[x * height + y]);
+            }
+        }
+    }
+
+    #[test]
+    fn test_double_fft() {
+        // Give it some awkward sizes. :)
+        let width = 13;
+        let height = 17;
+
+        let input = (0..(height * width)).map(|x| x as f64).collect::<Vec<_>>();
+
+        let input_complex = to_complex(&input);
+
+        let fft = fourier_transform(width, height, &input_complex, FftDirection::Forward);
+
+        let output_complex = fourier_transform(width, height, &fft, FftDirection::Inverse);
+
+        let output = from_complex(&output_complex);
+
+        assert_eq!(input.len(), output.len());
+        for (in_val, out_val) in input.iter().zip(output.iter()) {
+            assert!((in_val - out_val / (width * height) as f64).abs() < 1e-10);
+        }
+    }
+
+    #[test]
+    fn test_deconvolve() {
+        let (width, height) = (65, 65);
+
+        let (_, _, generated) = build_convolution_filter(width, height, 0.0);
+
+        let deconvolution_fft = fourier_transform(width, height, &to_complex(&generated), FftDirection::Forward).iter().map(Complex64::inv).collect::<Vec<_>>();
+
+        let fftd = fourier_transform(width, height, &to_complex(&generated), FftDirection::Forward);
+
+        let decon = fftd.iter().zip(deconvolution_fft.iter()).map(|(z1, z2)| z1 * z2).collect::<Vec<_>>();
+
+        let res = fourier_transform(width, height, &decon, FftDirection::Inverse);
+
+        let res2 = from_complex(&res).iter().map(|x| x / (width * height) as f64).collect::<Vec<_>>();
+
+        for (i, actual) in res2.iter().enumerate() {
+            let expected = if i == 0 { 1.0 } else { 0.0 };
+            assert!((actual - expected).abs() <= 1e-10);
+        }
+    }
+
+    // TODO: This function is messy, but we'll submit first and tidy later.
+    #[test]
+    fn test_end_to_end_deconvolve() {
+        // Choose numbers different from the image dimensions, to
+        // avoid missing bugs.
+        let rays = 35;
+        let angles = 40;
+
+        let src_img = image_load(Path::new("images/test.png"));
+        let scan = scan(&src_img, angles, rays);
+
+        let width = src_img.width;
+        let height = src_img.height;
+
+        let dst_img = reconstruct_convolution(&scan, width, height);
+
+
+        let (_, _, generated) = build_convolution_filter(width, height, 0.0);
+
+        let deconvolution_fft = fourier_transform(width, height, &to_complex(&generated), FftDirection::Forward)
+            .iter()
+            .map(|z| if z.norm_sqr() < 1e-4 { Complex64::new(0.0, 0.0) } else { z.inv() })
+            .collect::<Vec<_>>();
+
+        // TODO: Maybe use something that doesn't convert to matrix...
+        let img = dst_img.data.iter().map(|x| *x as f64).collect::<Vec<f64>>();
+
+        let fftd = fourier_transform(width, height, &to_complex(&img), FftDirection::Forward);
+
+        let decon = fftd.iter().zip(deconvolution_fft.iter()).map(|(z1, z2)| z1 * z2).collect::<Vec<_>>();
+
+        let res = fourier_transform(width, height, &decon, FftDirection::Inverse);
+
+        let res2 = from_complex(&res).iter().map(|x| x / (width * height) as f64).collect::<Vec<_>>();
+
+        let res3 = shift(width, height, &res2, width / 2, height / 2);
+
+        let recon_as_u8: DVector<u8> = DVector::from_iterator(width * height, res3
+            .iter()
+            .map(|x| x.max(0.0).min(255.0) as u8));
+
+        let img = TomoImage { width, height, data: recon_as_u8 };
+
+/* TODO: For debugging...
+        image_save(Path::new("full_cycle.png"), img);
+
+        let diff_as_u8: DVector<u8> = DVector::from_iterator(width * height, res3
+            .iter().zip(src_img.data.iter())
+            .map(|(x, y)| {
+                 let diff = *x as i32 - *y as i32;
+                 (diff + 128).max(0).min(255) as u8 }));
+
+        let diff_img = TomoImage { width, height, data: diff_as_u8 };
+        image_save(Path::new("full_cycle_diff.png"), diff_img);
+*/
+
+        let total_error: f64 = src_img.data
+            .iter()
+            .zip(img.data.iter())
+            .map(|(&p1, &p2)| (p1 as f64 - p2 as f64).abs())
+            .sum();
+
+        let average_error = total_error / (dst_img.width * dst_img.height) as f64;
+
+        // TODO: This error is pretty huge, but small enough to mean
+        // the image is roughly right.
+        assert!(average_error < 20.0);
     }
 }
